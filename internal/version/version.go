796eea5efa6430bb77086c4f43cb08e90199f00a
William Moreno Reyes <williamjmorenor@gmail.com>
2026-06-14 00:17:09 +0000

    chore(version): declare release version
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

---
+++ internal/version/version.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: William Moreno Reyes CP | MBA
+// SPDX-License-Identifier: Apache-2.0
+
+package version
+
+// Version is the Admirald release version.
+var Version = "0.0.1alpha4"
